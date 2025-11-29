26e001c726ac1721db6406da99c014556c90be49
maffiakhateguaro <maffiakhateg@gmail.com>
2025-11-29 14:32:06 +0800

    Implement TodoList component

---
+++ src/components/TodoList.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import TodoItem from './TodoItem';
+
+const TodoList = ({ todos }) => (
+  <ul>
+    {todos.map(todo => <TodoItem key={todo.id} todo={todo} />)}
+  </ul>
+);
+
+export default TodoList;
